1f5e9e1b877998d7b66ee853fd6e67407654fff9
Beebz <nyvivianv+git@gmail.com>
2025-07-04 17:26:48 -0400

    added hairstyle options

--- javascript.js
+++ javascript.js
@@ -26,7 +26,8 @@ var hairTexture = [
     ' straight ',
     ' wavy ',
     ' curly ',
-    ' kinky '
+    ' kinky ',
+    ' poofy',
 ]
 
 // What direction do hair 'chunks' follow
@@ -39,10 +40,11 @@ var hairFlow = [
 
 // The shape of the end of a hair 'chunk'
 var hairEnds = [
-    ' blunt ',
+    ' square ',
     ' poofy ',
     ' fluffy ',
-    ' spikey '
+    ' spikey ',
+    ' curly ',
 ]
 
 // Hair that hangs in front of your ears, usually excluded from ponytails. Think Inuyasha
@@ -52,59 +54,71 @@ var hairTendrils = [
     ' same as hair length '
 ]
 
-var randomLength = Math.floor(Math.random() * (hairLength.length));
-var randomPart = Math.floor(Math.random() * (hairPart.length));
-var randomTexture = Math.floor(Math.random() * (hairTexture.length));
-var randomFlow = Math.floor(Math.random() * (hairFlow.length));
-var randomEnds = Math.floor(Math.random() * (hairEnds.length));
-var randomTendrils = Math.floor(Math.random() * (hairTendrils.length));
-// var randomPlus = Math.floor(Math.random() * (hairPlus.length));
+/* The variables contain the value of randomizing the options, like the dice you would roll.
+ */
+let randomLength = Math.floor(Math.random() * (hairLength.length));
+let randomPart = Math.floor(Math.random() * (hairPart.length));
+let randomTexture = Math.floor(Math.random() * (hairTexture.length));
+let randomFlow = Math.floor(Math.random() * (hairFlow.length));
+let randomEnds = Math.floor(Math.random() * (hairEnds.length));
+let randomTendrils = Math.floor(Math.random() * (hairTendrils.length));
 
-/* ${hairLength[randomLength]}
-${hairPart[randomPart]}
-${hairTexture[randomTexture]}
-${hairFlow[randomFlow]}
-${hairEnds[randomEnds]}
-${hairTendrils[randomTendrils]}
-${hairPlus[randomPlus]} */
+/* When the variable selects an option, that is then pushed to be applied on each list of options
+i.e.  `${Skill[Check Roll]}`
+at least thats how i understand it */
 
 return ` This <b>${hairTexture[randomTexture]}</b> hair goes to <b>${hairLength[randomLength]}</b> length with <b>${hairTendrils[randomTendrils]}</b> tendrils.<br>
 The flow of the hair <b>${hairFlow[randomFlow]}</b> with <b>${hairEnds[randomEnds]}</b> ends.<br>
 And the bangs are <b>${hairPart[randomPart]}</b> parted.<br>`;
+}
+
+function hairPlus() {
+
+/* Various types of additional hairs */
+var hairPlusNames = [
+    'ponytails',
+    'buns',
+    'puffs',
+    'ahoge',
+]
+
+/* How large the additional hair is */
+var hairPlusSize =[
+    'small',
+    'medium',
+    'big',
+]
+
+/* Where does the addition start on the head */
+var hairPlusY = [
+    'low',
+    'mid',
+    'high',
+]
+
+/* Where the Hair is located around the Head  */
+var hairPlusX = [
+    'left',
+    'right',
+    'center',
+    'twin',
+]
+
+let randomPlusNames = Math.floor(Math.random() * (hairPlusNames.length));
+let randomPlusSize = Math.floor(Math.random() * (hairPlusSize.length));
+let randomPlusY = Math.floor(Math.random() * (hairPlusY.length));
+let randomPlusX = Math.floor(Math.random() * (hairPlusX.length));
+
+return `<br>Optional:<br> The hairstyle also has <b>${hairPlusSize[randomPlusSize]} ${hairPlusY[randomPlusY]} ${hairPlusX[randomPlusX]} ${hairPlusNames[randomPlusNames]}</b> `
+
+/* return ` This <b>${hairTexture[randomTexture]}</b> hair goes to <b>${hairLength[randomLength]}</b> length with <b>${hairTendrils[randomTendrils]}</b> tendrils.<br>
+The flow of the hair <b>${hairFlow[randomFlow]}</b> with <b>${hairEnds[randomEnds]}</b> ends.<br>
+And the bangs are <b>${hairPart[randomPart]}</b> parted.<br>`; */
 
 }
 
 function newHair() {
-	document.getElementById('finalResult').innerHTML = hairStyle();
+	document.getElementById('finalResult').innerHTML = hairStyle() + hairPlus();
 }
-
-
-/* // Basic hairstyle additions theres definitely a better way to do this but have mercy for now
-* I know how to fix this now!!
-* Make seperate vars for high/low, twin/center, ponytail buns duh! ill fix this abomination later though lol
-var hairPlus = [
-    ' high twin ponytails ',
-    ' mid twin ponytails ',
-    ' low twin ponytails ',
-    ' high twin buns ',
-    ' mid twin buns ',
-    ' low twin buns ',
-    ' high left ponytail ',
-    ' mid left ponytail ',
-    ' low left ponytail ',
-    ' high center ponytail ',
-    ' mid center ponytail ',
-    ' low center ponytail ',
-    ' high right ponytail ',
-    ' mid right ponytail ',
-    ' low right ponytail ',
-    ' high left bun ',
-    ' mid left bun ',
-    ' low left bun ',
-    ' high center bun ',
-    ' mid center bun ',
-    ' low center bun ',
-    ' high right bun ',
-    ' mid right bun ',
-    ' low right bun '
-]; */
+/*  Still desire to know how to add weighted chances to certain conditions
+because the true randomness can be a little chaotic */
